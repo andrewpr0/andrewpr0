3e11efd76f63bbbf84575bd3a4233c485dc77679
Andrew <andressanca95@gmail.com>
2025-01-10 10:07:07 -0500

    SEO melitico

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,38 @@ export default function Layout({ children, onCategoryChange }) {
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
+        <title>Andrew Portfolio - Concept Art | Concept Art Colombia</title>
+        <meta name="description" content="Explora increíbles proyectos de concept art. Andrew Sánchez, artista profesional, colombia ofrece diseños únicos de concept art y más."></meta>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet" />
+
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify({
+                    "@context": "https://schema.org",
+                    "@type": "WebPage",
+                    "name": "Concept Art | AndrewPro - Ilustrador en Colombia",
+                    "description": "Portafolio de Andrew Sánchez (AndrewPro), ilustrador colombiano especializado en Concept Art y arte digital.",
+                    "url": "https://andrewpr0.art",
+                    "author": {
+                        "@type": "Person",
+                        "name": "Andrew Sánchez",
+                        "url": "https://andrewpr0.art",
+                        "sameAs": [
+                            "https://www.artstation.com/andrewpro",
+                            "https://www.instagram.com/an.drew.pro",
+                            "https://twitter.com/andrewpr0"
+                        ]
+                    },
+                    "keywords": [
+                        "concept art",
+                        "concept art colombia",
+                        "andrewpro",
+                        "andrewpr0",
+                        "jaime sanchez ilustrador",
+                        "ilustrador colombia"
+                    ],
+                    "image": "https://andrewpr0.art/images/portafolio.jpg"
+                }) }} />
+
       </head>
       <body>
         <div className="site-content">
---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://andrewpr0.art', // Cambia esto por el dominio de tu página
+  generateRobotsTxt: true,         // Crea automáticamente el archivo robots.txt
+  sitemapSize: 5000,               // Número máximo de URLs por archivo (opcional)
+};
